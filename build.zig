10aa16938c51f5dceec7d8f1960d14157b754a4d
Magomed <git@magomed.dev>
2026-06-29 14:35:41 +0200

    build: add build.zig for building compiler

---
+++ build.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "zlox_treewalker",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
+    });
+
+    b.installArtifact(exe);
+
+    const run_step = b.step("run", "Run the compiler");
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+
+    const exe_tests = b.addTest(.{
+        .root_module = exe.root_module,
+    });
+
+    const test_step = b.step("test", "Run tests");
+
+    const install_tests_cmd = b.addInstallArtifact(exe_tests, .{});
+    const test_cmd = b.addRunArtifact(exe_tests);
+    test_step.dependOn(&install_tests_cmd.step);
+    test_step.dependOn(&test_cmd.step);
+}
